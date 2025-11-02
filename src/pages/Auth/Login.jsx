7ebcc5a4415e18f8378aa7133250bc76a313b3c1
Ansh Sharma <anshsharma2463@gmail.com>
2025-11-02 20:10:48 +0530

    login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,16 +1,28 @@
 import React,{useState} from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
 function Login() {
   const [email,setEmail]=useState("");
   const [password,setPassword]=useState("");
   const [error,setError]=useState("");
-  const navigate=useNavigateseNavigate();
+  // const navigate=useNavigateseNavigate();
 
   //Handle Login Form submit
   const handleLogin=async(e)=>{
-
+    e.preventDefault();
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address");
+      return;
+    }
+    if(!password){
+      setError("Password enter the password");
+      return;
+    }
+    setError("");
+    // Proceed with login logic (e.g., API call)
+    // navigate('/dashboard');
   }
   return (
     <AuthLayout>
@@ -27,6 +39,20 @@ function Login() {
                 placeholder="john@example.com"
                 type="text"
               />
+              <Input
+                value={password}
+                onChange={({target})=> setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 characters"
+                type="password"
+              />
+              {error && <p className="text-red-500 text-xs pb-5">{error}</p>}
+              <button type="submit" className='btn-primary'>LOGIN</button>
+              <p className='text-[13px] text-slate-800 mt-3'>
+                Don't have an account? 
+                <Link to="/signUp" className='font-medium text-primary underline'>Sign Up 
+                </Link>
+              </p>
             </form>
         </div>
     </AuthLayout>
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
 function SignUp() {
+  const [profilePic,setProfilePic]=useState(null);
+  const [fullName,setFullName]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [error,setError]=useState(null);
+  // const navigate=useNavigate();
+
+  //Handle SignUp Form submit
+  const handleSignUp=async(e)=>{
+  }
   return (
-    <div>SignUp</div>
+    <AuthLayout>
+    </AuthLayout>
   )
 }
 
