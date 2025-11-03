ed931b0a992c67bff097d89dd34681c45a61514a
Ansh Sharma <anshsharma2463@gmail.com>
2025-11-03 18:56:26 +0530

    signup page frontend

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -3,6 +3,7 @@ import AuthLayout from '../../components/layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 function SignUp() {
   const [profilePic,setProfilePic]=useState(null);
   const [fullName,setFullName]=useState("");
@@ -22,6 +23,7 @@ function SignUp() {
           Join us today by entering your details below.
         </p>
         <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
           <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             <Input
               value={fullName}
@@ -47,6 +49,13 @@ function SignUp() {
                 />
               </div>
           </div>
+            {error && <p className="text-red-500 text-xs pb-5">{error}</p>}
+            <button type="submit" className='btn-primary'>SIGN UP</button>
+            <p className='text-[13px] text-slate-800 mt-3'>
+            Already have an account?
+            <Link to="/login" className='font-medium text-primary underline'>Login
+            </Link>
+            </p>
         </form>
       </div>
     </AuthLayout>
